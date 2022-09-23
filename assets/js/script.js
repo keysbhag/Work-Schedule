1f569e4497c5084b6156bebeb6afb159c0225d52
keyshawn bhagwandin <keyshawn.bhag@hotmail.com>
2022-09-23 16:30:23 -0400

    finished local storage function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,43 @@
 let currentDate = moment().format("dddd, MMMM Do YYYY");
 $("#currentDay").text(currentDate);
 
-let time9am = $('#9am');
-let time10am = $('#10am'); 
-let time11am = $('#11am'); 
-let time12pm = $('#12pm'); 
-let time1pm = $('#1pm'); 
-let time2pm = $('#2pm'); 
-let time3pm = $('#3pm'); 
-let time4pm = $('#4pm'); 
-let time5pm = $('#5pm');
 
-let btn9am = $('#9am-btn');
-let btn10am = $('#10am-btn');
-let btn11am = $('#11am-btn');
-let btn12pm = $('#12pm-btn');
-let btn1pm = $('#1pm-btn');
-let btn2pm = $('#2pm-btn');
-let btn3pm = $('#3pm-btn');
-let btn4pm = $('#4pm-btn');
-let btn5pm = $('#5pm-btn');
+setInterval(changeSchedule, 1000);
 
-btn9am.on('click',function (event) {
-    event.preventDefault();
+function changeSchedule() {
+    
+}
 
-    let addEvent = time9am.val();
 
-    console.log(addEvent);
 
-    if (!addEvent) {
-        console.log('No shopping item filled out in form!');
-        return;
+
+
+
+
+// Everything below handles saving events in the scheduler
+let saveBtn = $('.save-btn');
+
+init();
+
+function init () {
+
+    for (let j = 0; j < 9; j++) {
+        let storedVal = JSON.parse(localStorage.getItem('event'+j.toString()));
+
+        $("#"+j.toString()+"a").text(storedVal);
     }
+}
 
+saveBtn.on('click',function (event) {
+    let targetSaveBtn = event.target.id;
+
+    for (let i = 0; i < 9; i++) {
+        event.preventDefault();
+        
+        if (targetSaveBtn == i) {             
+            let addEvent = $("#"+i.toString()+"a");
+
+            localStorage.setItem('event'+i.toString(), JSON.stringify(addEvent.val()));
+        }
+    }
 })
